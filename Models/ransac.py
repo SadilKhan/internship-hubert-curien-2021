09a2997d3e5b49ae1519ec4d95e7f88e575e5860
MD. Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2021-05-27 21:18:23 +0200

    dev: fixed the bug of directories

--- Models/ransac.py
+++ Models/ransac.py
@@ -30,7 +30,7 @@ class ransac:
         self.root=Tk()
 
         self.root.title("Ransac Image Alignment")
-        self.root.geometry("400x400")
+        self.root.geometry("700x700")
 
         # buttons
         self.button()
@@ -38,14 +38,26 @@ class ransac:
     
     def button(self):
         self.choose_folder=Button(self.root,text="Choose Folder",fg="black",command=self.open_folder,disabledforeground="black")
+        self.label_folder=Label(self.root,text="Choose A folder where images are located",fg="black")
         self.open_source=Button(self.root,text="Choose Source",fg="black",command=self.open_source,disabledforeground="black")
         self.open_target=Button(self.root,text="Choose Target",fg="black",command=self.open_target,disabledforeground="black")
+        self.label_sc=Label(self.root,text="Choose Source and target manually",fg="black")
         self.start=Button(self.root,text="Start Alignment",fg="black",command=self.alignment)
+        self.import_ransac=Button(self.root,text="Import Ransac Flow",fg="black",command=self.import_ransac_fn)
 
-        self.choose_folder.place(x=10,y=10)
-        self.open_source.place(x=10,y=90)
-        self.open_target.place(x=10,y=50)
-        self.start.place(x=10,y=130)
+        self.import_ransac.place(x=200,y=10)
+        self.label_folder.place(x=10,y=70)
+        self.choose_folder.place(x=10,y=90)
+        self.open_source.place(x=400,y=90)
+        self.open_target.place(x=400,y=130)
+        self.label_sc.place(x=400,y=70)
+        self.start.place(x=200,y=170)
+        
+    def import_ransac_fn(self):
+        self.ransac_folder=filedialog.askdirectory(title="Choose the Ransac flow folder.")
+    
+    def save(self):
+        self.save_folder=filedialog.askdirectory(title="Choose where to save the folder.")
     
     def open_folder(self):
         self.folder=filedialog.askdirectory(title="Select A Folder")
@@ -60,12 +72,13 @@ class ransac:
         self.target=filedialog.askopenfilename(title="Select Target Image",filetypes=(("jpg Files","*.jpg"),("All Files","*.*")))
     
     def alignment(self):
+        self.save()
         self.target_image=np.asarray(Image.open(self.target),dtype=np.uint8)        
         for Is in self.sources:
-            final_image=alignment(Is,self.target)
-            name="/Users/ryzenx/other/"+It.split("/")[-1]
+            final_image=alignment(Is,self.target,self.ransac_folder)
+            name=self.save_folder+Is.split("/")[-1]
             cv2.imwrite(name,final_image)
-        target_name="/Users/ryzenx/other/"+It.split("/")[-1].split(".")[0]+"_ref.jpg"
+        target_name=self.save_folder+self.target.split("/")[-1].split(".")[0]+"_ref.jpg"
         cv2.imwrite(target_name,self.target_image)
 
 if __name__=="__main__":
