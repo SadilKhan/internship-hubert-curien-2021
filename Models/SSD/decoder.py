eda9caf3787bb01bc49fa8398e056187576252ee
MD. Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2021-06-15 14:42:32 +0200

    updated code
    -for getting feature maps locations for every object

--- Models/SSD/decoder.py
+++ Models/SSD/decoder.py
@@ -3,9 +3,10 @@ import numpy as np
 import torch
 import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.ops import RoIAlign
 from torchsummary import summary
-from box_utils import cxcy_to_gcxgcy,cxcy_to_xy,xy_to_cxcy,gcxgcy_to_cxcy,find_jaccard_overlap
+from SSD.box_utils import cxcy_to_gcxgcy,cxcy_to_xy,xy_to_cxcy,gcxgcy_to_cxcy,find_jaccard_overlap
 
 class Decoder(nn.Module):
   def __init__(self,detector=None,output_size=(5,5),num_filter=50000):
@@ -27,41 +28,53 @@ class Decoder(nn.Module):
     
 
     # Decoding Layers
-    self.convT_1=nn.ConvTranspose2d(self.num_filter,1024,kernel_size=3,stride=2) #(256,39,39)
-    self.convT_2=nn.ConvTranspose2d(1024,512,kernel_size=3,stride=2) #(128,79,79)
-    self.upsample_1=nn.Upsample((80,80)) #(128,80,80)
+    self.convT_1=nn.ConvTranspose2d(self.num_filter,1024,kernel_size=3,stride=2) #(1024,11,11)
+    self.convT_2=nn.ConvTranspose2d(1024,512,kernel_size=3,stride=2,padding=2) #(512,19,19)
+    self.convT_3=nn.ConvTranspose2d(512,256,kernel_size=3,stride=2) #(256,39,39)
 
-    self.convT_3=nn.ConvTranspose2d(512,128,kernel_size=3,stride=2)
-    self.upsample_2=nn.Upsample((160,160))
+    self.convT_4=nn.ConvTranspose2d(256,128,kernel_size=3,stride=2) # (128,79,79)
+    self.convT_5=nn.ConvTranspose2d(128,64,kernel_size=5,stride=2) # (64, 161, 161)
 
-    self.convT_4=nn.ConvTranspose2d(128,32,kernel_size=3,stride=2)
-    self.convT_5=nn.ConvTranspose2d(32,3,kernel_size=1,stride=2)
-    self.upsample_3=nn.Upsample((640,640))
+    self.convT_6=nn.ConvTranspose2d(64,32,kernel_size=1,stride=2) # (32, 321, 321)
+    self.convT_7=nn.ConvTranspose2d(32,3,kernel_size=1,stride=2) # (3, 641, 641)
+    self.upsample_1=nn.Upsample((640,640)) # (3,640,640)
 
-  def forward(self,mask):
-    x,self.boxes=mask
+
+  def forward(self,x,boxes=None):
+    self.boxes=boxes
 
     # Get RoI's offset
-    self.locs,_=self.detector(x)
+    self.locs,self.classes=self.detector(x)
+    self.classes=self.classes.cpu().detach()
+    self.locs=self.locs.cpu().detach()
 
+    # Transfer ssd style coordinates to xy coordinates
+    self.__batch_size=self.locs.size(0)
+    for i in range(self.__batch_size):
+      self.locs[i]=cxcy_to_xy(gcxgcy_to_cxcy(self.locs[i],self.detector.priors_cxcy.cpu()))
 
     # Get all the feature maps
-    x=self.get_feature_maps(x)
+    self.feature_map=self.get_feature_maps(x)
 
-    out=self.convT_1(x)
-    out=self.convT_2(out)
+    out=F.relu(self.convT_1(self.feature_map))
+    out=F.relu(self.convT_2(out))
+    out=F.relu(self.convT_3(out))
+    out=F.relu(self.convT_4(out))
+    out=F.relu(self.convT_5(out))
+    out=F.relu(self.convT_6(out))
+    out=F.relu(self.convT_7(out))
     out=self.upsample_1(out)
-    out=self.convT_3(out)
-    out=self.upsample_2(out)
-    out=self.convT_4(out)
-    out=self.convT_5(out)
-    out=self.upsample_3(out)
     return out
   
   def get_feature_maps(self,x):
     """ Get all the feature maps from the ssd detector"""
-    self.rescale_factors = nn.Parameter(torch.cuda.FloatTensor(1, 512, 1, 1))  # there are 512 channels in conv4_3_feats
     
+    self.rescale_factors = nn.Parameter(torch.cuda.FloatTensor(1, 512, 1, 1))  # there are 512 channels in conv4_3_feats
+    # ROI Postions in Locs data Dictinary
+    self.__roi_pos_dict={"conv4_3":[0,38*38],"conv7":[38*38,38*38+19*19],
+                 "conv8_2":[38*38+19*19,38*38+19*19+10*10],"conv9_2":[38*38+19*19+10*10,38*38+19*19+10*10+5*5],
+                 "conv10_2":[38*38+19*19+10*10+5*5, 38*38+19*19+10*10+5*5+3*3]}
+    self.feature_map_size={"conv4_3":38,"conv7":19,"conv8_2":10,"conv9_2":5,"conv10_2":3}
 
     # Get Conv4 and Conv7 from VGG
     self.conv4_3,self.conv7=self.detector.base(x)
@@ -71,48 +84,57 @@ class Decoder(nn.Module):
     self.conv4_3 = self.conv4_3 / norm  # (N, 512, 38, 38)
     self.conv4_3 = self.conv4_3 * self.rescale_factors 
 
-    # Get rest of the feature maps from
+    # Get rest of the feature maps from conv7
     self.conv8_2, self.conv9_2, self.conv10_2, self.conv11_2 = self.detector.aux_convs(self.conv7)
 
+
     # Merge all the Feature maps after roi aligning
+    self.locations=self.get_map_for_objects()
     feature_map=self.merge_feature_maps()
     return feature_map
+  
+  def get_map_for_objects(self):
+    # Get the locations of objects in feature maps
+    locations=[]
+    for i in range(self.__batch_size):
+      sc_bx,cl_bx=self.classes[i].max(dim=1)
+      un_cl=torch.unique(cl_bx)
+      # The locations of objects in every feature maps
+      class_dict={}
+      for num,k in enumerate(un_cl):
+        if k!=0:
+          new_bx_k=[]
+          bx_k=(cl_bx==k).nonzero(as_tuple=True)[0].cpu()
+          for rp in list(self.__roi_pos_dict.keys()):
+            min_,max_=self.__roi_pos_dict[rp]
+            max_bx_k=bx_k[bx_k<max_]
+            range_bx_k=max_bx_k[max_bx_k>=min_]
+            try:
+              locs_for_k=torch.clamp(self.locs[i][range_bx_k[torch.argmax(sc_bx[range_bx_k]).item()].item()],min=0,max=1)*self.feature_map_size[rp]
+              new_bx_k+=[dict({rp:locs_for_k.tolist()})]
+            except:
+              new_bx_k+=[]
+          class_dict[k.cpu().item()]=new_bx_k
+      locations.append(class_dict)
+    return locations
     
   
   def merge_feature_maps(self):
     """ Merge all Fearure Maps"""
-    # ROI Postions in Locs data Dictinary
-    self.__roi_pos_dict={"conv4_3":[0,5776],"conv7":[5776,7942],
-                 "conv8_2":[7942,8542],"conv9_2":[8542,8692],"conv10_2":[8692,8728],
-                 "conv11_2":[8728,8732]}
-
-    """# Region of Interest Dictionary
-    self.__roi_dict={"conv4_3":self.locs[:,:5776,:],"conv7":self.locs[:,5776:7942,:],"conv8_2":self.locs[:,7942:8542,:],
-             "conv9_2":self.locs[:,8542:8692,:],"conv10_2":self.locs[:,8692:8728,:],"conv11_2":self.locs[:,8728:8732,:]}"""
 
     # Feature Maps Dictionary
     self.__feat_map_dict={"conv4_3":self.conv4_3,"conv7":self.conv7,
                  "conv8_2":self.conv8_2,"conv9_2":self.conv9_2,"conv10_2":self.conv10_2,
                  "conv11_2":self.conv11_2}
-    
-    # Find Roi's for conv4_3, since we will use roialign here
-    self.__roi_conv4_3=self.roi_align("conv4_3",(38,38))
-    self.__roi_conv7=self.roi_align("conv7",(19,19))
-    self.__roi_conv8_2=self.roi_align("conv8_2",(10,10))
-    self.__roi_conv9_2=self.roi_align("conv9_2",(5,5))
-    self.__roi_conv10_2=self.roi_align("conv10_2",(3,3))
-    #self.__roi_conv11_2=self.roi_align("conv11_2",(3,3))
 
-    feature_map=torch.cat((self.__roi_conv4_3,self.__roi_conv7,self.__roi_conv8_2,self.__roi_conv9_2,self.__roi_conv10_2),dim=1)
+    self.feature_map=[]
+    #assert self.__batch_size==len(locations)
+    for i in range(self.__batch_size):
+      for j in range(self.locations[i]):
+        pass
 
-    # Since Feature maps dimensions are different for every image, we will take first 50000 filters
-    if feature_map.size(1)>=self.num_filter:
-      feature_map=feature_map[:,:self.num_filter,:,:]
-    else:
-      # We need to upsample
-      upsample=nn.Upsample(5,self.num_filter)
-      feature_map=torch.moveaxis(upsample(torch.moveaxis(feature_map,1,-1)),-1,1)
-    assert feature_map.shape==torch.Size([self.__batch_size,self.num_filter,self.output_size[0],self.output_size[1]])
+    feature_map=torch.cat((self.__roi_conv4_3,self.__roi_conv7,self.__roi_conv8_2,self.__roi_conv9_2,self.__roi_conv10_2),dim=0)
+
 
     
     return feature_map
@@ -125,7 +147,7 @@ class Decoder(nn.Module):
   
   def find_roi(self,feature_name,feature_size):
     """ It finds the RoI's for a specific feature name"""
-    self.__batch_size=self.locs.size(0)
+    
     roi_sp=[]
     min_,max_=self.__roi_pos_dict[feature_name]
     
@@ -150,7 +172,6 @@ class Decoder(nn.Module):
     self.__roi=self.find_roi(feature_name,feature_size)
     aligned_map=self.roialign(self.__feat_map_dict[feature_name],self.__roi)
     return aligned_map.reshape(self.__batch_size,-1,w,h)
-
 
 
 if __name__=="__main__":
