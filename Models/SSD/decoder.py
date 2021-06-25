2729dc944e59c15342cd0a61b832a34a4ba03a0b
MD. Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2021-06-25 19:05:55 +0200

    dev: updated decoder

--- Models/SSD/decoder.py
+++ Models/SSD/decoder.py
@@ -15,29 +15,24 @@ class Decoder(nn.Module):
     detector --> Object Detector
     output_size --> Output size of ROI Align step. Default (5,5)
     """
-    # Decoding Layers
-    self.convT_1=nn.ConvTranspose2d(2560,1024,kernel_size=3,stride=2) #(1024, 11, 11)
-    self.convT_2=nn.ConvTranspose2d(1024,512,kernel_size=3,stride=2) #(512, 23, 23)
-    self.convT_3=nn.ConvTranspose2d(512,256,kernel_size=3,stride=1) #(256, 25, 25)
-
-    self.convT_4=nn.ConvTranspose2d(256,128,kernel_size=3,stride=2) # (128, 51, 51)
-    self.convT_5=nn.ConvTranspose2d(128,3,kernel_size=3,stride=2) # (3, 103, 103)
-    self.upsample_1=nn.Upsample((100,100)) # (3, 100, 100)
-
+    self.decoder=nn.Sequential(
+        nn.ConvTranspose2d(1024,256,kernel_size=5,stride=2,output_padding=1),
+        nn.ReLU(),
+        nn.ConvTranspose2d(256,32,kernel_size=3,stride=2,output_padding=1),
+        nn.ReLU(),
+        nn.Upsample((32,32)),
+        nn.ConvTranspose2d(32,16,kernel_size=3,stride=2,padding=1,output_padding=1),
+        nn.ReLU(),
+        nn.ConvTranspose2d(16,3,kernel_size=3,stride=2,padding=1,output_padding=1),
+        nn.Sigmoid()
+    )
 
   def forward(self,x):
     # Output Images for batches
     self.__batch_size=len(x)
     output=[]
     for i in range(self.__batch_size):
-      out=F.relu(self.convT_1(x[i]))
-      out=F.relu(self.convT_2(out))
-      out=F.relu(self.convT_3(out))
-      out=F.relu(self.convT_4(out))
-      out=F.relu(self.convT_5(out))
-      #out=F.relu(self.convT_6(out))
-      #out=F.relu(self.convT_7(out))
-      out=self.upsample_1(out)
+      out=self.decoder(x[i])
       output.append(out)
 
     return output
